110248429516f93c11b670b8ebbae0e444d66bc8
DesireForSmth <alexander.setrov@gmail.com>
2020-04-27 17:27:49 +0300

    Initial Commit

---
+++ GeoChecker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeoChecker
+//
+//  Created by Александр Сетров on 27.04.2020.
+//  Copyright © 2020 Александр Сетров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
